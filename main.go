eb30d8612541848a0eefe6bba668693fab8a476b
Arthur <arthurvaverko@gmail.com>
2024-10-20 16:02:42 +0300

    inint wip

---
+++ main.go
@@ -0,0 +1,56 @@
+// main.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type Character struct {
+	Name    string
+	HP      int
+	MaxHP   int
+	Mana    int
+	MaxMana int
+}
+
+type GameState struct {
+	StoryLines []string
+	Character  Character
+}
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	// Serve static files
+	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Game routes
+	r.Get("/", handleIndex)
+
+	http.ListenAndServe(":3000", r)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	gameState := GameState{
+		StoryLines: []string{
+			"You find yourself in a dimly lit ancient temple.",
+			"Intricate carvings cover the weathered stone walls.",
+			"A mysterious altar glows faintly in the distance.",
+			"The air is thick with the scent of age-old incense.",
+		},
+		Character: Character{
+			Name:    "John Constantine",
+			HP:      100,
+			MaxHP:   100,
+			Mana:    75,
+			MaxMana: 100,
+		},
+	}
+
+	component := Layout(gameState)
+	component.Render(r.Context(), w)
+}
